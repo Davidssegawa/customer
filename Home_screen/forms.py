dc91e13fbc6460259c26211b848d5d8a97d143b0
unknown <ssegawajoe8@gmail.com>
2024-03-30 14:18:08 +0300

    Plotting

--- Home_screen/forms.py
+++ Home_screen/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class PlotForm(forms.Form):
-    start = forms.DateField(widget=forms.DateInput(attrs={'type':'date'}))
-    end = forms.DateField(widget=forms.DateInput(attrs={'type':'date'}))
+    start = forms.DateField(widget=forms.DateInput(attrs={'type':'timestamp'}))
+    end = forms.DateField(widget=forms.DateInput(attrs={'type':'timestamp'}))
                             
