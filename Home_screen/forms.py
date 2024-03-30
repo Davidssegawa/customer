7b46791f9adb104b08694544148104e608ca7711
unknown <ssegawajoe8@gmail.com>
2024-03-30 16:54:40 +0300

    Plotting

--- Home_screen/forms.py
+++ Home_screen/forms.py
@@ -2,5 +2,5 @@ from django import forms
 from django.forms import DateTimeInput
 
 class DateRangeForm(forms.Form):
-    start_timestamp = forms.DateTimeField(label='Start Timestamp', widget=DateTimeInput(attrs={'type': 'datetime-local'}))
-    end_timestamp = forms.DateTimeField(label='End Timestamp', widget=DateTimeInput(attrs={'type': 'datetime-local'}))
+    start_timestamp = forms.DateTimeField(label='start_timestamp', widget=DateTimeInput(attrs={'type': 'datetime-local'}))
+    end_timestamp = forms.DateTimeField(label='end_timestamp', widget=DateTimeInput(attrs={'type': 'datetime-local'}))
