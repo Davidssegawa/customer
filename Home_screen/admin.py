a4f9bab472ea7f8d19cf27fe6d32568a8033eda6
unknown <ssegawajoe8@gmail.com>
2024-04-01 13:08:54 +0300

    Payment.html

--- Home_screen/admin.py
+++ Home_screen/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import WaterUnit
+from .models import WaterUnit, Meter_data
 # Register your models here.
 admin.site.register(WaterUnit)
+admin.site.register(Meter_data)
